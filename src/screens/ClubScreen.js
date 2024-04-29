092b10ecfa97ff4d4a1fb0262e311c436f72a382
Soonly-T <intell1slt@gmail.com>
2024-04-29 23:06:04 +0700

    remove auto update

--- src/screens/ClubScreen.js
+++ src/screens/ClubScreen.js
@@ -1,6 +1,6 @@
 import { collection, getDocs } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
-import { FlatList, Text, View, TouchableOpacity } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { db } from '../../firebaseConfig';
 import ClubItem from '../components/ClubItem';
 import { globalStyles } from '../styles/GlobalStyles.js';
@@ -16,16 +16,12 @@ const ClubDataScreen = () => {
 
   useEffect(() => {
     fetchClubs(); // Fetch clubs when the component mounts
-
-    const intervalId = setInterval(fetchClubs, 500); // Fetch clubs every 30 seconds
-
-    return () => clearInterval(intervalId); // Clean up the interval on unmount
   }, []);
 
   return (
     <View>
       <View style={globalStyles.header}>
-      <Text style={globalStyles.SubHTitle}>Clubs</Text>
+        <Text style={globalStyles.SubHTitle}>Clubs</Text>
       </View>
       <FlatList 
         data={clubs}
